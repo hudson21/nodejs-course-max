fbefc80a323e24b3a38b51459191c75b6ab18403
hudson21 <camihudsondiaz@ittepic.edu.mx>
2019-02-07 21:07:21 -0600

    64. Limiting Middleware Execution to POST Requests

--- second-project/app.js
+++ second-project/app.js
@@ -1,14 +1,37 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-//Use allows us to use middlewares
-app.use((req, res, next) => {
-    console.log('In the middleware');
+//This function registers a middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', (req, res, next) => {
+    console.log('This always runs');
+    next();
 });
 
-const server = http.createServer(app);
+//Use allows us to use middlewares
+/*app.use((req, res, next) => {
+    console.log('In the middleware');
+    next();// this allows the request to continue to the next middlewarein line 
+});*/
 
-server.listen(3000);
+app.use('/add-product', (req, res, next) => {
+    //Send a response
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    //Send a response
+    res.send('<h1>Hello from Express</h1>');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
