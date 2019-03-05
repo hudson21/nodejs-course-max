94d9bea31ff53c4df65ff4c3464bfae6ca649bab
hudson21 <camihudsondiaz@ittepic.edu.mx>
2019-03-04 21:45:18 -0600

    Storing the User in our Database

--- second-project/app.js
+++ second-project/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/404');
 const mongoConnect = require('./helpers/database').mongoConnect;
+const User = require('./models/user');
 
 //This is for express-handlerbars 
 //const expressHbs = require('express-handlebars');
@@ -35,17 +36,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));//The user will be able to access to the public folder 
 // This only returns a middleware. For incoming requests this function is gonne be executed
 app.use((req, res, next) => {
-    /*User.findById(1)
+    User.findById('5c7defcd349c852d8c025a64')
     .then(user => {
         req.user = user;//Here we are adding a new field to out request object
         console.log('user', user);
         next();//We can continue with out next step
     })
-    .catch(err => console.log(err));*/
-    next();
+    .catch(err => console.log(err));
 });
 
 //Routes
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
