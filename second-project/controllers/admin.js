484856ad6d7a33a82b6d678aa34c1f4e2a8708fe
carlos.hudson <carlos.hudson@hp.com>
2019-03-07 10:32:13 -0600

    191. Storing the User in our Database 02:29

--- second-project/controllers/admin.js
+++ second-project/controllers/admin.js
@@ -20,7 +20,7 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    const product = new Product(title, price, description, imageUrl);
+    const product = new Product(title, price, description, imageUrl, null);
     product.save()
     .then(result => { 
         //console.log(result);
