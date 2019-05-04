bb8f013c8ea9b4c977e9249382a7efb996142d48
hudson21 <camihudsondiaz@ittepic.edu.mx>
2019-05-04 13:29:28 -0500

    Finished the module of pagination

--- second-project/controllers/shop.js
+++ second-project/controllers/shop.js
@@ -6,18 +6,34 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
-    .then(products => {
-      console.log(products);
+  .countDocuments()
+  .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)//Pagination
+        .limit(ITEMS_PER_PAGE)//Pagination 
+  })
+  .then(products => {
       res.render('shop/product-list', {
-        prods: products,
-        pageTitle: 'All Products',
+        prods: products, 
+        pageTitle: 'Products',
         path: '/products',
-        isAuthenticated: req.session.isLoggedIn
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
-    .catch(err => {
+  .catch(err => {
       const error = new Error(err);
       error.httpStatusCode = 500;
       return next(error);
@@ -43,15 +59,31 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
-    .then(products => {
+  .countDocuments()
+  .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)//Pagination
+        .limit(ITEMS_PER_PAGE)//Pagination 
+  })
+  .then(products => {
       res.render('shop/index', {
-        prods: products,
+        prods: products, 
         pageTitle: 'Shop',
-        path: '/'
+        path: '/',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
-    .catch(err => {
+  .catch(err => {
       const error = new Error(err);
       error.httpStatusCode = 500;
       return next(error);
