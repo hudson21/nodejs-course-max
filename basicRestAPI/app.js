3c1df229c7301b2594fd9e2a737ea3a954fe76ad
hudson21 <camihudsondiaz@ittepic.edu.mx>
2019-05-07 23:25:45 -0500

    changes

--- basicRestAPI/app.js
+++ basicRestAPI/app.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+    next(); 
 });
 
 app.use('/feed', feedRoutes);
